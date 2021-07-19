fb0672137eabe22f1f297cac77a4a706c6f946e3
chaitalizn <phchaitali3@gmail.com>
2021-07-19 18:35:21 -0400

    schemas directory setup

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
