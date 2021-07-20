0eee15502e5857521d529dafd1296519dc48f31f
chaitalizn <phchaitali3@gmail.com>
2021-07-20 00:09:51 -0400

    User login and create user Query and Mutation created with JWT

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,63 @@
+const { signToken } = require('../utils/auth');
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+  Query: {
+    helloWorld: () => {
+      return 'Hello world!';
+    },
+    me: async (parent, { username }) => {
+      return User.findOne({ username })
+      .select('-__v -password')
+      .populate('savedBooks');
     }
+  }
+  ,
+  Mutation: {
+      //create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if(!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const correctPw = await user.isCorrectPassword(password)
+
+        if(!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const token = signToken(user);
+        return { token, user };
+        
+      }
+      }
+    //     //login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+    //      // {body} is destructured req.body
+
+    //     //save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+    //     // user comes from `req.user` created in the auth middleware function
+
+    //      //remove a book from `savedBooks`
+
+    // }
   };
   
   module.exports = resolvers;
+
+//   Query: {
+//     //get a single user by either their id or their username
+//   me: async (parent, { username }) => {
+//     return User.findOne({ username })
+//       .select('-__v -password')
+//       .populate('savedBooks');
+//   }
+// }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,57 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Query {
-    helloWorld: String
-  }
-  `;
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
 
+type Book {
+  bookId(bookId: String): Book
+  authors: [String]
+  description: String
+  title: String
+  image: Boolean
+  link: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  helloWorld: String
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+}
+
+`;
 // export the typeDefs
 module.exports = typeDefs;
+
+// type Mutation: {
+//   login(email: String!, password: String!): Auth
+//   addUser(username: String!, email: Sting!, password: String!): Auth
+// }
+
+
+// type Auth {
+//   token: String
+//   user: User
+// }
+
+
+
+// type Query {
+//     me: User
+//   }
+
+//   `;
